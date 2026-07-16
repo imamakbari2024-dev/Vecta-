4f6d30f96e31fe34071abcc7263b68ac31628dcc
imamakbari2024-dev <imamakbari.2024@student.uny.ac.id>
2026-07-17 02:09:12 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // Wajib ditambahkan untuk tombol Terang/Gelap
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
